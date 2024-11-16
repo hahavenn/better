f1bb232032afeda8f588a87c8de09068d1824d0c
hahavenn <hahavenn@yandex.ru>
2024-11-17 00:12:24 +0300

    update: nuxt config

--- user_main/nuxt.config.ts
+++ user_main/nuxt.config.ts
@@ -1,5 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+
+  app: {
+    head: {
+      meta: [
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+        },
+        {
+          charset: "utf-8",
+        },
+      ],
+      link: [
+        {
+          rel: "icon",
+          href: "favicon.ico",
+        },
+      ],
+    },
+  },
+
+  devServer: {
+    host: "localhost",
+    port: 3000,
+  },
+});
