23db65c07f6a5eb125a0f6b98848883c703b86c7
hahavenn <hahavenn@yandex.ru>
2024-11-17 00:54:39 +0300

    fix: eslint rule with single word component name

--- eslint.config.js
+++ eslint.config.js
@@ -5,16 +5,34 @@ import pluginVue from "eslint-plugin-vue";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,vue}"] },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,vue}"],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...pluginVue.configs["flat/essential"],
   {
     files: ["**/*.vue"],
-    languageOptions: { parserOptions: { parser: tseslint.parser } },
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
   },
   {
     ignores: ["**/.nuxt/", "**/node_modules/", "**/.output/"],
+  },
+  {
+    rules: {
+      "vue/multi-word-component-names": "off",
+    },
   },
 ];
