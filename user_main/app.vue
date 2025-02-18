5861556e9e3536b7319b90a8a663b621cfcccf78
hahavenn <hahavenn@yandex.ru>
2025-02-18 23:14:27 +0300

    fix: alias for root directory

--- user_main/app.vue
+++ user_main/app.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script setup lang="ts">
-import HeaderApp from "@/components/app/Header/HeaderApp.vue";
+import HeaderApp from "~/components/app/Header/HeaderApp.vue";
 </script>
 
 <style scoped>
