f690400b8ac7c39afd72ad517213e285602cc15e
hahavenn <hahavenn@yandex.ru>
2025-02-18 23:16:15 +0300

    add: state types for project

---
+++ user_main/types/state.ts
@@ -0,0 +1,22 @@
+/**
+ * State types for everything in project (components, requests and etc.)
+ */
+export type StateTypes =
+  | "default"
+  | "hover"
+  | "active"
+  | "focus"
+  | "disabled"
+  | "visited"
+  | "checked"
+  | "selected"
+  | "expanded"
+  | "collapsed"
+  | "open"
+  | "closed"
+  | "loading"
+  | "error"
+  | "success"
+  | "warning"
+  | "info"
+  | "danger";
