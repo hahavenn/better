2edac6897e0b37ad4ad11b73175f2399d3c97a49
hahavenn <hahavenn@yandex.ru>
2025-02-23 21:09:14 +0300

    add: rel values for link elements

---
+++ user_main/types/link_rel_attribute.ts
@@ -0,0 +1,31 @@
+/**
+ * All possible rel values for links
+ *
+ * @see https://html.spec.whatwg.org/multipage/links.html#linkTypes
+ */
+export type LinkRelAttribute =
+  | "alternate"
+  | "author"
+  | "bookmark"
+  | "canonical"
+  | "dns-prefetch"
+  | "expect"
+  | "external"
+  | "help"
+  | "icon"
+  | "license"
+  | "manifest"
+  | "modulepreload"
+  | "nofollow"
+  | "noopener"
+  | "noreferrer"
+  | "opener"
+  | "pingback"
+  | "preconnect"
+  | "prefetch"
+  | "preload"
+  | "privacy-policy"
+  | "search"
+  | "stylesheet"
+  | "tag"
+  | "terms-of-service";
