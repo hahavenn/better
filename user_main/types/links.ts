0902e03852b2b6c78b21fb28be77cbfcd064af19
hahavenn <hahavenn@yandex.ru>
2025-02-23 21:09:34 +0300

    add: presets for links in project

---
+++ user_main/types/links.ts
@@ -0,0 +1,7 @@
+/**
+ * All possible link types' snippets in project, that can be re-used
+ *
+ * @description
+ * - `home`: represents home page of project (root)
+ */
+export type LinkTypes = "home";
