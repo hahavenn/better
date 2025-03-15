b10885bc806fc4e82c73ad1a07aafd234fde9608
hahavenn <hahavenn@yandex.ru>
2025-03-16 00:29:01 +0300

    update: user theme plugin

--- user_main/plugins/set_user_theme.client.ts
+++ user_main/plugins/set_user_theme.client.ts
@@ -1,21 +1,15 @@
+// todo - re-do with nuxt module for theme (https://nuxt.com/modules/color-mode)
 /**
  * This plugin is used to set user theme
  */
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.hook("app:beforeMount", () => {
-    const themePreference = localStorage.getItem("themePreference");
-    const htmlElement = document.getElementsByTagName("html")[0];
-
-    if (themePreference) {
-      htmlElement.classList.add(themePreference);
-      return;
-    }
-
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      htmlElement.classList.add("dark");
-      localStorage.setItem("themePreference", "dark");
-    } else {
-      localStorage.setItem("themePreference", "light");
-    }
+    document
+      .getElementsByTagName("html")[0]
+      .classList.add(
+        window.matchMedia("(prefers-color-scheme: light)").matches
+          ? "light"
+          : "dark"
+      );
   });
 });
