7ac4ee63810231e7cf75c3e01cbf45e148dd73b1
hahavenn <hahavenn@yandex.ru>
2025-03-19 22:22:34 +0300

    add: pages for site sections

---
+++ user_main/pages/help/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <UiContainer> This is help page </UiContainer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ user_main/pages/profile/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <UiContainer> This is profile page </UiContainer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
