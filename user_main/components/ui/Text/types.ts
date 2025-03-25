ab400ecba623c2d8752d328fbe0153eb13127350
hahavenn <hahavenn@yandex.ru>
2025-03-25 19:28:23 +0300

    add: plain text type

--- user_main/components/ui/Text/types.ts
+++ user_main/components/ui/Text/types.ts
@@ -5,8 +5,9 @@ import type { PaddingRule } from "~/components/ui/types";
  * Pre-defined types for text in different use cases
  * @description
  * - `link`: represents link text with no specified config/a11y (link component already specified it)
+ * - `plain`: represents plain text without specified config/a11y
  */
-export type TextTypes = "link";
+export type TextTypes = "link" | "plain";
 
 export type TextUIProps = {
   type: TextTypes;
