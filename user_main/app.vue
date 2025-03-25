5ba32174a134dbc994db0fca0def4c31844d7eaa
hahavenn <hahavenn@yandex.ru>
2025-03-25 20:48:41 +0300

    remove: unused code

--- user_main/app.vue
+++ user_main/app.vue
@@ -1,20 +1,8 @@
 <template>
   <AppHeader />
-  <main>
-    <NuxtPage />
-  </main>
+  <NuxtPage />
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped>
-main {
-  width: 100%;
-  height: 100%;
-
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  gap: var(--offset__0);
-}
-</style>
+<style scoped></style>
