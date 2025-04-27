ccbdb83efbbf2dd629ba7f10c90a5c43428c813e
hahavenn <hahavenn@yandex.ru>
2025-04-27 13:21:12 +0300

    in progress

--- user_main/components/app/Header/index.vue
+++ user_main/components/app/Header/index.vue
@@ -16,6 +16,13 @@
         </UiLink>
 
         <AppSearch />
+
+        <UiDropdown :title="'Site sections'">
+          <UiDropdownItem> Hello! </UiDropdownItem>
+          <UiDropdownItem> Hello1! </UiDropdownItem>
+          <UiDropdownItem> Hello2! </UiDropdownItem>
+          <UiDropdownItem> Hello3! </UiDropdownItem>
+        </UiDropdown>
       </header>
     </UiContainer>
   </div>
---
+++ user_main/components/ui/Dropdown/item.vue
@@ -0,0 +1,9 @@
+<template>
+  <li tabindex="0">
+    <slot></slot>
+  </li>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
