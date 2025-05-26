995f05dc64b9b0c5dfb24df1cab14477c23944bf
hahavenn <hahavenn@yandex.ru>
2025-05-26 14:09:17 +0300

    add: constants for color palettes

---
+++ better_client/constants/colors.ts
@@ -0,0 +1,29 @@
+/**
+ * All color palettes, that used in project.
+ * @see https://tailwindcss.com/docs/colors
+ */
+const COLOR_PALETTES = {
+  RED: "red",
+  ORANGE: "orange",
+  AMBER: "amber",
+  YELLOW: "yellow",
+  LIME: "lime",
+  GREEN: "green",
+  EMERALD: "emerald",
+  TEAL: "teal",
+  CYAN: "cyan",
+  SKY: "sky",
+  BLUE: "blue",
+  INDIGO: "indigo",
+  VIOLET: "violet",
+  PURPLE: "purple",
+  FUCHSIA: "fuchsia",
+  PINK: "pink",
+  ROSE: "rose",
+  SLATE: "slate",
+  GRAY: "gray",
+  ZINC: "zinc",
+  NEUTRAL: "neutral",
+  STONE: "stone",
+};
+export { COLOR_PALETTES };
