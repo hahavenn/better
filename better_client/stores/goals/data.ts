12048ff6054f8e6227b60d704e4f68b0386c6013
hahavenn <hahavenn@yandex.ru>
2025-07-06 20:16:35 +0300

    update: move test data to separate file out of store

---
+++ better_client/stores/goals/data.ts
@@ -0,0 +1,199 @@
+import type { UserGoal } from "./types";
+
+const goal: UserGoal = {
+  name: "Some user goal",
+  description: "some description about goal",
+  complete: false,
+  steps: [
+    {
+      name: "Learn React",
+      description: "some description about react",
+      complete: true,
+      subSteps: [
+        {
+          name: "react_1",
+          description: "description react_1",
+          complete: true,
+        },
+        {
+          name: "react_2 lorema sahsa hsa assaja sajsk a akakakakak k a akjs jasbcsjkdbc asbc kaskjcb  kjascj kdscjk sdjk cnj ksdncjk  cnnsdncdcs cncnz zncznz ",
+          description:
+            "description react_2wqwqwi wqjoiwqd wqdijq wdjo wqjqwjijwdqj wqdjjwdjdw d  d jdjd jd jdjd jdjdjdjj d",
+          complete: true,
+        },
+        {
+          name: "react_3",
+          description: "description react_3",
+          complete: true,
+        },
+        {
+          name: "react_4",
+          description: "description react_4",
+          complete: true,
+        },
+        {
+          name: "react_1",
+          description: "description react_1",
+          complete: true,
+        },
+        {
+          name: "react_2",
+          description: "description react_2",
+          complete: true,
+        },
+        {
+          name: "react_3",
+          description: "description react_3",
+          complete: true,
+        },
+        {
+          name: "react_4",
+          description: "description react_4",
+          complete: true,
+        },
+        {
+          name: "react_1",
+          description: "description react_1",
+          complete: true,
+        },
+        {
+          name: "react_2",
+          description: "description react_2",
+          complete: true,
+        },
+        {
+          name: "react_3",
+          description: "description react_3",
+          complete: true,
+        },
+        {
+          name: "react_4",
+          description: "description react_4",
+          complete: true,
+        },
+        {
+          name: "react_1",
+          description: "description react_1",
+          complete: true,
+        },
+        {
+          name: "react_2",
+          description: "description react_2",
+          complete: true,
+        },
+        {
+          name: "react_3",
+          description: "description react_3",
+          complete: true,
+        },
+        {
+          name: "react_4",
+          description: "description react_4",
+          complete: true,
+        },
+        {
+          name: "react_1",
+          description: "description react_1",
+          complete: true,
+        },
+        {
+          name: "react_2",
+          description: "description react_2",
+          complete: true,
+        },
+        {
+          name: "react_3",
+          description: "description react_3",
+          complete: true,
+        },
+        {
+          name: "react_4",
+          description: "description react_4",
+          complete: true,
+        },
+        {
+          name: "react_1",
+          description: "description react_1",
+          complete: true,
+        },
+        {
+          name: "react_2",
+          description: "description react_2",
+          complete: true,
+        },
+        {
+          name: "react_3",
+          description: "description react_3",
+          complete: true,
+        },
+        {
+          name: "react_4",
+          description: "description react_4",
+          complete: true,
+        },
+        {
+          name: "react_1",
+          description: "description react_1",
+          complete: true,
+        },
+        {
+          name: "react_2",
+          description: "description react_2",
+          complete: true,
+        },
+        {
+          name: "react_3",
+          description: "description react_3",
+          complete: true,
+        },
+        {
+          name: "react_4",
+          description: "description react_4",
+          complete: true,
+        },
+      ],
+    },
+    {
+      name: "pwa",
+      description: "some description about pwa",
+      complete: true,
+      subSteps: [],
+    },
+    {
+      name: "Learn vue",
+      description: "some description about vue",
+      complete: false,
+      subSteps: [
+        {
+          name: "vue_1",
+          description: "description vue_1",
+          complete: true,
+        },
+        {
+          name: "vue_2",
+          description: "description vue_2",
+          complete: false,
+        },
+      ],
+    },
+    {
+      name: "bun_1",
+      description: "some description about bun_1",
+      complete: false,
+      subSteps: [],
+    },
+    {
+      name: "bun_2",
+      description: "some description about bun_2",
+      complete: false,
+      subSteps: [],
+    },
+    {
+      name: "bun_3",
+      description: "some description about bun_3",
+      complete: false,
+      subSteps: [],
+    },
+  ],
+};
+export { goal };
