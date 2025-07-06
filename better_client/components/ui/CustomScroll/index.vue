4e4012dc0a2279366ff86fdaaa62642aef752076
hahavenn <hahavenn@yandex.ru>
2025-07-06 20:15:32 +0300

    add: custom scroll

---
+++ better_client/components/ui/CustomScroll/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="custom_scroll">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import COLOR_BORDER_ACTIVE from "~/constants/color_border_active";
+import type { ColorPalettes } from "~/types/color_palettes";
+
+const props = defineProps<{
+  palette: ColorPalettes;
+}>();
+
+const isDark = useDark();
+
+const scrollbarColor = computed(
+  () => `var(${COLOR_BORDER_ACTIVE[props.palette][isDark ? "DARK" : "LIGHT"]})`
+);
+</script>
+
+<style scoped>
+.custom_scroll {
+  /* Firefox */
+  scrollbar-width: thin;
+  scrollbar-color: v-bind("scrollbarColor") transparent; /* scrollbar + bg */
+
+  /* Webkit */
+  &::-webkit-scrollbar {
+    width: var(--border-radius__1);
+    height: var(--border-radius__1);
+    background-color: transparent;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: v-bind("scrollbarColor"); /* scrollbar */
+    border-radius: var(--border-radius__1);
+  }
+}
+</style>
---
+++ better_client/components/ui/CustomScroll/readme.md
@@ -0,0 +1,11 @@
+# CustomScroll
+
+This component is used for provide custom scroll to scrollable elements relative to used color palette
+
+## Usage
+
+```html
+<UiCustomScroll :palette="'RED'">
+  <ul class="max-h-[200px]overflow-auto"></ul>
+</UiCustomScroll>
+```
