dd20a4873ec66bd5580a30c4e00d57d426f18a0b
hahavenn <hahavenn@yandex.ru>
2025-07-07 07:33:10 +0300

    add: fn to update goal completeness

--- better_client/stores/goals/index.ts
+++ better_client/stores/goals/index.ts
@@ -1,11 +1,10 @@
+import type { UserGoalSubStep, UserGoal, UserGoalStep } from "./types";
+
 import { goal } from "./data";
-import type { UserGoal } from "./types";
 
 const useGoalsStore = defineStore("goalsStore", () => {
   const _goals = ref<UserGoal[]>([goal]);
-  /**
-   * All user goals
-   */
+  /** All user goals */
   const goals = computed(() => _goals.value);
 
   /**
@@ -42,10 +41,82 @@ const useGoalsStore = defineStore("goalsStore", () => {
     );
   }
 
+  /**
+   * Update goal completeness.
+   *
+   * Cases:
+   * - `goalId` provided -> all steps' and subSteps' `complete` state will be set equals goal state
+   * - `goalId`, `stepId`, provided -> only step and itself subSteps `complete` state will be changed
+   * - `goalId`, `stepId`, `subStepId` provided -> update only subStep state
+   *
+   * @returns No return value provided. For updates you should look for `goals` variable
+   */
+  function updateGoalCompleteness(
+    params: {
+      goalId: UserGoal["id"];
+      stepId?: UserGoalStep["id"];
+      subStepId?: UserGoalSubStep["id"];
+    },
+    state = false
+  ) {
+    for (const goal of _goals.value) {
+      // wrong goal
+      if (goal.id !== params.goalId) continue;
+
+      // whole goal should be changed
+      if (params.stepId === undefined) {
+        goal.complete = state;
+
+        // these cycles updates whole goal, so we don't need to check all steps and subSteps
+        goal.steps.forEach((step) => {
+          step.subSteps.forEach((subStep) => {
+            subStep.complete = state;
+          });
+          step.complete = state;
+        });
+      } else {
+        for (const step of goal.steps) {
+          // wrong step
+          if (step.id !== params.stepId) continue;
+
+          // whole step should be updated
+          if (params.subStepId === undefined) {
+            step.complete = state;
+
+            step.subSteps.forEach((subStep) => {
+              subStep.complete = state;
+            });
+          } else {
+            for (const subStep of step.subSteps) {
+              // wrong subStep
+              if (subStep.id !== params.subStepId) continue;
+
+              // update subStep
+              subStep.complete = state;
+            }
+          }
+
+          // check for whole step completed
+          if (!Boolean(step.subSteps.find((subStep) => !subStep.complete))) {
+            step.complete = true;
+          }
+        }
+
+        // check for whole goal completed
+        if (!Boolean(goal.steps.find((step) => !step.complete))) {
+          goal.complete = true;
+        }
+      }
+    }
+  }
+
   return {
     goals,
+
     calcTotalParts,
     calcCompletedParts,
+
+    updateGoalCompleteness,
   };
 });
 
