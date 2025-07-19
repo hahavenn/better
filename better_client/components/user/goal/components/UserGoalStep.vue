15aeeae6a402b434835b6d55d62bf590a39da3c8
hahavenn <hahavenn@yandex.ru>
2025-07-20 01:09:41 +0300

    update: user goal step

--- better_client/components/user/goal/components/UserGoalStep.vue
+++ better_client/components/user/goal/components/UserGoalStep.vue
@@ -1,16 +1,19 @@
 <template>
   <li
-    class="flex flex-col gap-(--offset__6) rounded-(--border-radius__0) border-(length:--border-width__0) border-solid px-(--offset__0) py-(--offset__4)"
+    class="flex h-fit flex-col gap-(--offset__6) rounded-(--border-radius__0) border-(length:--border-width__0) border-solid px-(--offset__0) py-(--offset__4)"
     :class="liClasses"
   >
     <header class="flex w-full items-center justify-between gap-(--offset__6)">
-      <h2
-        class="line-clamp-1 text-2xl font-semibold"
-        :style="{
-          textDecoration: props.step.complete ? 'line-through' : '',
-        }"
-      >
-        {{ props.step.name }}
+      <h2 class="flex items-center gap-(--offset__5) text-2xl font-semibold">
+        <span
+          class="line-clamp-1"
+          :style="{
+            textDecoration: props.step.complete ? 'line-through' : '',
+          }"
+        >
+          {{ props.step.name }}
+        </span>
+        <span>({{ props.step.subSteps.length }})</span>
       </h2>
 
       <UiCheckbox
@@ -21,13 +24,19 @@
       />
     </header>
 
-    <UiCustomScroll
-      v-if="step.subSteps.length > 0"
-      :palette="palette"
+    <UiAccordion
+      v-show="isExpandable && accordionExpanded"
+      v-model="accordionExpanded"
+      ref="accordionRef"
+      :label="'step'"
+      :accordionId="`step_${props.step.id}`"
     >
-      <ul class="flex max-h-[200px] flex-col gap-(--offset__5) overflow-auto">
+      <ul
+        v-show="accordionExpanded"
+        class="flex max-h-full flex-col gap-(--offset__5) overflow-auto"
+      >
         <li
-          v-for="sub in step.subSteps"
+          v-for="sub in props.step.subSteps"
           :key="sub.id"
         >
           <UserGoalSubStep
@@ -37,7 +46,33 @@
           />
         </li>
       </ul>
-    </UiCustomScroll>
+    </UiAccordion>
+
+    <div class="flex h-9 w-full items-center gap-1">
+      <button
+        v-if="isExpandable"
+        @click="accordionExpanded = !accordionExpanded"
+        ref="stepExpandBtnRef"
+        class="flex h-full shrink-0 grow cursor-pointer items-center justify-center rounded-tl-lg rounded-bl-lg border-(length:--border-width__0) border-solid"
+        :class="stepBtnClasses"
+        type="button"
+      >
+        <UiIcon
+          :icon="'expand'"
+          :palette="palette"
+        />
+      </button>
+      <button
+        class="flex h-full shrink-0 grow cursor-pointer items-center justify-center border-(length:--border-width__0) border-solid"
+        :class="[...stepBtnClasses, ...stepAddBtnClasses]"
+        type="button"
+      >
+        <UiIcon
+          :icon="'add'"
+          :palette="palette"
+        />
+      </button>
+    </div>
   </li>
 </template>
 
@@ -48,12 +83,16 @@ import COLOR_PALETTES_STYLES from "~/constants/color_palettes";
 
 import useGoalsStore from "~/stores/goals";
 
+import type { UiAccordionType } from "~/components/ui/Accordion/types";
+import useAccordion from "~/components/ui/Accordion/useAccordion";
+
 import { goalIdKey } from "~/components/user/goal/provide_inject";
 
 import UiCheckbox from "~/components/ui/Checkbox/UiCheckbox.vue";
+import UiIcon from "~/components/ui/Icon/UiIcon.vue";
 
-const UiCustomScroll = defineAsyncComponent(
-  () => import("~/components/ui/CustomScroll/UiCustomScroll.vue")
+const UiAccordion = defineAsyncComponent(
+  () => import("~/components/ui/Accordion/UiAccordion.vue")
 );
 const UserGoalSubStep = defineAsyncComponent(
   () => import("./UserGoalSubStep.vue")
@@ -93,13 +132,27 @@ watch(
     }
   }
 );
+
+const isExpandable = computed(() => props.step.subSteps.length > 0);
+const accordionRef = useTemplateRef<UiAccordionType>("accordion");
+const stepExpandBtnRef = useTemplateRef<HTMLButtonElement>("stepExpandBtnRef");
+const { expanded: accordionExpanded } = useAccordion(
+  accordionRef,
+  stepExpandBtnRef
+);
+
+const stepBtnClasses = [
+  COLOR_PALETTES_STYLES[palette].BORDER.DEFAULT,
+  COLOR_PALETTES_STYLES[palette].BG.DEFAULT,
+
+  COLOR_PALETTES_STYLES[palette].BORDER.HOVER,
+  COLOR_PALETTES_STYLES[palette].BG.HOVER,
+];
+const stepAddBtnClasses = computed(() => [
+  isExpandable.value
+    ? "rounded-tr-lg rounded-br-lg"
+    : "rounded-tl-lg rounded-bl-lg rounded-tr-lg rounded-br-lg",
+]);
 </script>
 
-<style scoped>
-li:nth-child(odd) {
-  grid-row: auto / span 3;
-}
-li:nth-child(even) {
-  grid-row: auto / span 2;
-}
-</style>
+<style scoped></style>
