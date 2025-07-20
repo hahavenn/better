3c2240f767eba9f1d968219f7e8205808d3e6d46
hahavenn <hahavenn@yandex.ru>
2025-07-20 11:29:36 +0300

    fix: warning with same names

--- better_client/nuxt.config.ts
+++ better_client/nuxt.config.ts
@@ -2,13 +2,6 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
 
-  components: [
-    {
-      path: "~/components",
-      pathPrefix: false,
-    },
-  ],
-
   app: {
     head: {
       meta: [
