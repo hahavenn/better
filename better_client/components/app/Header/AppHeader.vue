b3fb03263fc8541992fd72c087c995245fd9ad74
hahavenn <hahavenn@yandex.ru>
2025-07-24 13:31:53 +0300

    fix: header structure

--- better_client/components/app/Header/AppHeader.vue
+++ better_client/components/app/Header/AppHeader.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="fixed top-0 z-[1] flex w-full flex-col items-center">
+  <header class="fixed top-0 z-[1] flex w-full flex-col items-center">
     <UiContainer
       :responsive="true"
       :attachedTo="'attachment__top'"
       :fullWidth="true"
     >
-      <header class="flex w-full flex-row items-center gap-(--offset__3)">
+      <div class="flex w-full flex-row items-center gap-(--offset__3)">
         <UiLink :preset="'home'">
           <span class="text_link text-2xl font-bold underline"> better </span>
         </UiLink>
 
         <AppSearch />
-      </header>
+      </div>
     </UiContainer>
-  </div>
+  </header>
 </template>
 
 <script lang="ts" setup></script>
