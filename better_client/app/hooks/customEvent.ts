5db2a933fe5ebb9cdfedb918d815f84ff201e347
hahavenn <hahavenn@yandex.ru>
2025-09-07 12:38:56 +0300

    chore: hook for customEvents

--- better_client/app/hooks/customEvent.ts
+++ better_client/app/hooks/customEvent.ts
@@ -2,23 +2,65 @@ export type CustomEventListener<EventDetail> = (
   detail: CustomEvent<EventDetail>
 ) => void;
 
+/**
+ * Hook to work with custom events
+ *
+ * @param customEvent string name of custom event
+ *
+ * @returns `listen` function to add listener for custom event with options
+ * @returns `dispatch` function to dispatch custom event with detail data
+ * @returns `remove` function to remove saved listener from custom event
+ *
+ * ------
+ *
+ * @example
+ * ```typescript
+ * const { listen, dispatch, remove } = useCustomEvent("my_custom_event");
+ *
+ * // add listener for event
+ * listen(() => {
+ *   console.log("event listener")
+ * }, {
+ *   // listener options
+ * })
+ *
+ * // dispatch event (for any action)
+ * dispatch({
+ *   // event data to be handled
+ * })
+ *
+ * // remove listener
+ * remove()
+ * ```
+ */
 export default function useCustomEvent<EventDetail>(customEvent: string) {
-  /** start listen to CustomEvent */
-  function listen(listener: CustomEventListener<EventDetail>) {
-    window.addEventListener(customEvent, (event: Event) => {
-      if (event instanceof CustomEvent) {
-        listener(event as CustomEvent<EventDetail>);
-      }
-    });
+  let eventListener: ((...args: any) => void) | null = null;
+
+  function listen(
+    listener: CustomEventListener<EventDetail>,
+    options?: EventListenerOptions
+  ) {
+    // save listener for further remove
+    eventListener = listener;
+
+    window.addEventListener(
+      customEvent,
+      (event: Event) => {
+        if (event instanceof CustomEvent) {
+          listener(event as CustomEvent<EventDetail>);
+        }
+      },
+      options
+    );
   }
 
-  /** dispatch CustomEvent */
-  function dispatch(detail: EventDetail) {
-    if (import.meta.dev)
+  function dispatch(detail?: EventDetail) {
+    if (import.meta.dev) {
       console.debug(
         `custom event [${customEvent}] dispatched with detail`,
         detail
       );
+    }
 
     window.dispatchEvent(
       new CustomEvent<EventDetail>(customEvent, {
@@ -27,14 +69,16 @@ export default function useCustomEvent<EventDetail>(customEvent: string) {
     );
   }
 
-  /** remove CustomEvent listener */
-  function remove(listener: (...args: any) => void) {
-    window.removeEventListener(customEvent, listener);
+  function remove() {
+    if (eventListener) window.removeEventListener(customEvent, eventListener);
   }
 
   return {
+    /** start listen to CustomEvent */
     listen,
+    /** dispatch CustomEvent */
     dispatch,
+    /** remove CustomEvent listener */
     remove,
   };
 }
