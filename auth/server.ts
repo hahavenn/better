2c90f38f4d38f3c64f4357b66ef93a18e2030a06
hahavenn <hahavenn@yandex.ru>
2025-09-11 21:37:41 +0300

    feat: add certificates for https

--- auth/server.ts
+++ auth/server.ts
@@ -1,8 +1,23 @@
 import processEnv from "./processEnv.ts";
 
+import fs from "node:fs";
+import path, { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
 import Fastify from "fastify";
 
-const fastify = Fastify({ logger: processEnv.DEV === "true" });
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const fastify = Fastify({
+  logger: processEnv.DEV === "true",
+
+  http2: true,
+  https: {
+    allowHTTP1: true,
+    key: fs.readFileSync(path.join(__dirname, "https", "private.key")),
+    cert: fs.readFileSync(path.join(__dirname, "https", "certificate.crt")),
+  },
+});
 
 fastify.get("/", async (request, reply) => {
   return { hello: "world" };
