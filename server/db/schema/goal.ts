e7789afda16fbf92cb1240d6f10729925b54ffca
hahavenn <hahavenn@yandex.ru>
2025-10-18 17:21:08 +0300

    feat: update goal schema

--- server/db/schema/goal.ts
+++ server/db/schema/goal.ts
@@ -1,16 +1,27 @@
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { usersTable } from "./user";
 
 // #region goal
 export const goalsTable = sqliteTable("goals_table", {
   /**
+   * User identifier, that owns goal
+   *
+   * @todo add see link in docs
+   * @see `server/db/schema/user.ts`
+   */
+  user: text()
+    .references(() => usersTable.login)
+    .notNull(),
+
+  /**
    * Unique uuid identifier of user goal
    */
-  id: text().primaryKey().unique(),
+  id: text().unique().notNull(),
 
   /**
    * Name of user goal
    */
-  name: text(),
+  name: text().notNull(),
 
   /**
    * Description of user goal
@@ -24,22 +35,29 @@ export const stepsTable = sqliteTable("steps_table", {
   /**
    * Unique uuid identifier of goal's step
    */
-  id: text().primaryKey().unique(),
+  id: text().unique().notNull(),
 
   /**
    * Foreign identifier of relative goal
    */
-  goalId: text().references(() => goalsTable.id),
+  goalId: text()
+    .references(() => goalsTable.id)
+    .notNull(),
 
   /**
    * Name of user step
    */
-  name: text(),
+  name: text().notNull(),
 
   /**
    * Description of user step
    */
   description: text(),
+
+  /**
+   * Complete state of step
+   */
+  complete: integer({ mode: "boolean" }).notNull(),
 });
 // #endregion step
 
@@ -48,21 +66,28 @@ export const subStepsTable = sqliteTable("sub_steps_table", {
   /**
    * Unique uuid identifier of steps's sub-step
    */
-  id: text().primaryKey().unique(),
+  id: text().unique().notNull(),
 
   /**
    * Foreign identifier of relative step
    */
-  stepId: text().references(() => stepsTable.id),
+  stepId: text()
+    .references(() => stepsTable.id)
+    .notNull(),
 
   /**
    * Name of user sub-step
    */
-  name: text(),
+  name: text().notNull(),
 
   /**
    * Description of user sub-step
    */
   description: text(),
+
+  /**
+   * Complete state of sub-step
+   */
+  complete: integer({ mode: "boolean" }).notNull(),
 });
 // #endregion subStep
