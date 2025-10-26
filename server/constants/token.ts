cee30fb6030677a666528fe287df3e0d3e63da02
hahavenn <hahavenn@yandex.ru>
2025-10-26 20:49:05 +0300

    feat: reduce toke lifetime

--- server/constants/token.ts
+++ server/constants/token.ts
@@ -2,7 +2,7 @@
  * Expiration times for different tokens in ms
  */
 const TOKEN_EXPIRATION_TIMES = {
-  ACCESS: 60 * 60 * 24 * 1000,
-  REFRESH: 60 * 60 * 24 * 7 * 1000,
+  ACCESS: 60 * 15 * 1000,
+  REFRESH: 60 * 60 * 24 * 1 * 1000,
 } as const;
 export default TOKEN_EXPIRATION_TIMES;
