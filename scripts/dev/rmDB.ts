5471d335c3e992251218e7993b855ae1cdaeb471
hahavenn <hahavenn@yandex.ru>
2025-11-06 22:13:12 +0300

    refactor: remove unnecessary path resolve call for db path

--- scripts/dev/rmDB.ts
+++ scripts/dev/rmDB.ts
@@ -6,7 +6,7 @@ import path from "node:path";
  * This script is used to remove main file of database
  */
 (async function () {
-  const rmPath = path.resolve(path.resolve("."), "server", "db", "main.db");
+  const rmPath = path.resolve(".", "server", "db", "main.db");
   rm(rmPath).catch(() => console.log("no file"));
 })();
 //#endregion
