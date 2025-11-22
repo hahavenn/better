4fe7ed7a0831970b9a6435403d103ef76f364fe3
hahavenn <hahavenn@yandex.ru>
2025-11-23 00:43:20 +0300

    feat: add reset database function for correct testing process

---
+++ tests/helpers/resetDb.ts
@@ -0,0 +1,11 @@
+import useFetch from "../../app/hooks/useFetch";
+
+/**
+ * Creates request to reset all databases for correct testing process
+ */
+export default async function () {
+  await useFetch({
+    url: "/api/admin/reset/db",
+    baseUrl: process.env["SERVER_BASE_URL"],
+  });
+}
