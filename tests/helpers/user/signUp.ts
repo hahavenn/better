6de5614a3b13bb7b3e630cc321a988da54a4b14b
hahavenn <hahavenn@yandex.ru>
2025-11-23 12:07:02 +0300

    feat: return response info from signUpUser test helper

--- tests/helpers/user/signUp.ts
+++ tests/helpers/user/signUp.ts
@@ -1,5 +1,8 @@
 import useFetch from "../../../app/hooks/useFetch";
 
+import type { AuthSignupResponse } from "../../../shared/types/response/auth/signup";
+import type { ErrorResponse } from "../../../shared/types/response/error";
+
 /**
  * Sign up user for testing purposes
  * @param user to sign up
@@ -8,10 +11,16 @@ export default async function signUpUser(user: {
   login: string;
   password: string;
 }) {
-  await useFetch({
+  const response = await useFetch<AuthSignupResponse, ErrorResponse>({
     url: "/api/auth/sign_up",
     baseUrl: process.env["SERVER_BASE_URL"],
     method: "POST",
     body: user,
   });
+
+  if (!response || "message" in response) {
+    throw `Invalid response - ${JSON.stringify(response)}`;
+  }
+
+  return response;
 }
