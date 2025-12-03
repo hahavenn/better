1530d4121a76ba3c4f7d83cca99d09ce24d50aa6
hahavenn <hahavenn@yandex.ru>
2025-12-03 22:54:39 +0300

    refactor: generators

--- scripts/generators/css/palettes/_.ts
+++ scripts/generators/css/palettes/_.ts
@@ -1,64 +1,60 @@
-import fs from "node:fs";
-import path from "node:path";
+import { readFile } from "node:fs/promises";
+import { resolve } from "node:path";
 
 import generateColorPalettesType from "./type.ts";
 import generateColorEntity from "./entity.ts";
 import generatePalettesStyles from "./styles.ts";
 
-export default function runPalettesGenerators() {
-  fs.readFile(
-    path.join(process.cwd(), "app", "assets", "css", "colors", "index.css"),
-    "utf-8",
-    (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-
-      // const cssVars = data.match(/--plt-[a-zA-Z]+-[a-zA-Z]+-[a-zA-Z]+-[a-zA-Z]+/g);
-      // console.log("cssVars", cssVars);
-
-      const palettes = [...data.matchAll(/\/\*(.*?)\*\//g)]
-        ?.map((m) => m[1]?.trim())
-        .filter((p) => p !== undefined);
-      // console.log("palettes", palettes);
-
-      const themes = [
-        ...new Set(
-          [
-            ...data.matchAll(
-              /--plt-[a-zA-Z]+-[a-zA-Z]+-([a-zA-Z]+)-[a-zA-Z]+/g
-            ),
-          ].map((match) => match[1])
-        ),
-      ].filter((t) => t !== undefined);
-      // console.log("themes", themes);
-
-      const entities = [
-        ...new Set(
-          [
-            ...data.matchAll(
-              /--plt-[a-zA-Z]+-([a-zA-Z]+)-[a-zA-Z]+-[a-zA-Z]+/g
-            ),
-          ].map((match) => match[1])
-        ),
-      ].filter((e) => e !== undefined);
-      // console.log("entities", entities);
-
-      const states = [
-        ...new Set(
-          [
-            ...data.matchAll(
-              /--plt-[a-zA-Z]+-[a-zA-Z]+-[a-zA-Z]+-([a-zA-Z]+)/g
-            ),
-          ].map((match) => match[1])
-        ),
-      ].filter((s) => s !== undefined);
-      // console.log("states", states);
-
-      generateColorPalettesType(palettes);
-      generateColorEntity(entities, themes, states, palettes);
-      generatePalettesStyles(entities, themes, states, palettes);
-    }
+export default async function runPalettesGenerators() {
+  const cssContent = await readFile(
+    resolve(process.cwd(), "app", "assets", "css", "colors", "index.css"),
+    "utf-8"
   );
+
+  // const cssVars = cssContent.match(
+  //   /--plt-[a-zA-Z]+-[a-zA-Z]+-[a-zA-Z]+-[a-zA-Z]+/g
+  // );
+  // console.log("cssVars", cssVars);
+
+  const palettes = [...cssContent.matchAll(/\/\*(.*?)\*\//g)]
+    ?.map((m) => m[1]?.trim())
+    .filter((p) => p !== undefined);
+  // console.log("palettes", palettes);
+
+  const themes = [
+    ...new Set(
+      [
+        ...cssContent.matchAll(
+          /--plt-[a-zA-Z]+-[a-zA-Z]+-([a-zA-Z]+)-[a-zA-Z]+/g
+        ),
+      ].map((match) => match[1])
+    ),
+  ].filter((t) => t !== undefined);
+  // console.log("themes", themes);
+
+  const entities = [
+    ...new Set(
+      [
+        ...cssContent.matchAll(
+          /--plt-[a-zA-Z]+-([a-zA-Z]+)-[a-zA-Z]+-[a-zA-Z]+/g
+        ),
+      ].map((match) => match[1])
+    ),
+  ].filter((e) => e !== undefined);
+  // console.log("entities", entities);
+
+  const states = [
+    ...new Set(
+      [
+        ...cssContent.matchAll(
+          /--plt-[a-zA-Z]+-[a-zA-Z]+-[a-zA-Z]+-([a-zA-Z]+)/g
+        ),
+      ].map((match) => match[1])
+    ),
+  ].filter((s) => s !== undefined);
+  // console.log("states", states);
+
+  generateColorPalettesType(palettes);
+  generateColorEntity(entities, themes, states, palettes);
+  generatePalettesStyles(entities, themes, states, palettes);
 }
--- scripts/generators/index.ts
+++ scripts/generators/index.ts
@@ -1,5 +1,6 @@
 import runCssGenerators from "./css/index.ts";
+import generateIconMap from "./icons/generateIconMap.ts";
 
-const generatorsToRun = [runCssGenerators];
+const generatorsToRun = [runCssGenerators, generateIconMap];
 
 generatorsToRun.forEach((g) => g());
